7cffbe37f718101b229e16904d5e17c7c08ca032
rirufa <rirufa@c-fo.com>
2016-06-07 20:28:40 +0900

    スクレイピングツールを作った

---
+++ NicoNicoScraper/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoNicoScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = MainAsync(args);
+            t.Wait();
+        }
+        static async Task MainAsync(string[] args)
+        {
+            var email = "";
+            var pass = "";
+            var query = "";
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Enter Email: ");
+                email = Console.ReadLine();
+                Console.WriteLine("Enter Password: ");
+
+                // Mask Password
+                pass = "";
+                ConsoleKeyInfo key;
+                do
+                {
+                    key = Console.ReadKey(true);
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        pass += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key != ConsoleKey.Backspace || pass.Length <= 0) continue;
+                        pass = pass.Substring(0, (pass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                }
+                while (key.Key != ConsoleKey.Enter);
+                Console.WriteLine();
+
+                Console.WriteLine("query: ");
+                query = Console.ReadLine();
+            }
+            else
+            {
+                email = args[0];
+                pass = args[1];
+                query = args[3];
+            }
+
+            Scraper scraper = new Scraper();
+            await scraper.Login(email, pass);
+            foreach(var search in scraper.Scrape(query))
+            {
+                Console.WriteLine(string.Format("{0}:{1},{2}",search.ContentId,search.Title,search.StartTime));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NicoNicoScraper/Scraper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NicoNico.Net.Entities.Search;
+using NicoNico.Net.Managers;
+
+namespace NicoNicoScraper
+{
+    class Scraper
+    {
+        const string config_file_name = "last_scraped";
+        const string date_format = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+        NicoNico.Net.Entities.User.UserSession session;
+        System.Net.CookieContainer cookieContainer;
+        DateTime lastDateTime;
+
+        public Scraper()
+        {
+            if (File.Exists(config_file_name))
+                this.lastDateTime = DateTime.Parse(File.ReadAllText(config_file_name),null,System.Globalization.DateTimeStyles.RoundtripKind);
+            else
+                this.lastDateTime = new DateTime(2006,12,12);   //ニコニコ動画の開始日以降を指定する
+
+        }
+
+        public async Task Login(string email, string pass)
+        {
+            var authManager = new AuthenticationManager();
+            var userLoginSession = await authManager.LoginUserThroughV1ApiAsync(email, pass);
+            this.cookieContainer = authManager.CreateLoginCookieContainer(userLoginSession);
+
+            // Reset the authentication manager with the proper cookie container.
+            authManager = new AuthenticationManager(cookieContainer);
+            this.session = await authManager.StartUserSessionAsync();
+
+            // In order to test "extending" the users session, we need to reset the authManager again,
+            // this time with the session key.
+            authManager = new AuthenticationManager(cookieContainer, session.Session);
+
+            var test = await authManager.ExtendUserSessionAsync();
+        }
+
+        public IEnumerable<Search> Scrape(string query)
+        {
+            SearchManager search_manager = new SearchManager(cookieContainer, session.Session);
+            for(int i = 0; i < 16; i++)
+            {
+                var search_task = search_manager.Search(
+                    SearchBuilder.Build(
+                        SearchType.Video, query, NicoNicoTarget.Keyword, NicoNicoSort.StartTime, false)
+                        .Offset(0)
+                        .Limit(100)
+                        .Range(NicoNicoFilter.StartTime,NicoNicoFilterOperator.Gte,this.lastDateTime)
+                    );
+                var search_result = search_task.Result;
+                foreach (var search in search_result.Data)
+                    yield return search;
+            }
+
+            using (StreamWriter sw = new StreamWriter(config_file_name))
+            {
+                sw.WriteLine(DateTime.Now.ToString(date_format));
+            }
+        }
+    }
+}
